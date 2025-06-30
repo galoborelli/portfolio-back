0162f921a3ccadb24c712a0e1e72b5c55a620b22
galob <borelligalo19@gmail.com>
2025-06-30 09:42:23 +0200

    dump database portfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -97,11 +97,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',          # nombre de la base de datos que creaste
+        'USER': 'postgres',           # usuario de PostgreSQL
+        'PASSWORD': 'postgres',    # reemplazalo con tu contraseña real
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
